0e0625f97a175d1c03a9fe989d3bd863eeacc3e2
Steve <stigzler@msn.com>
2026-08-27 09:43:48 +0100

    Default settling time chnage to 5s

--- src/spool-dat-torrent.core/Configuration/GlobalSpoolSettings.cs
+++ src/spool-dat-torrent.core/Configuration/GlobalSpoolSettings.cs
@@ -46,7 +46,7 @@ namespace SpoolDatTorrent.Core.Configuration
 
         /// <summary>Time to wait after pausing before moving files, in seconds.</summary>
         [Range(1, 480)]
-        public int SettlingTimeSeconds { get; set; } = 3;
+        public int SettlingTimeSeconds { get; set; } = 5;
 
         /// <summary>
         /// Directory where per-stream copies of the .torrent and .dat files are cached,
